0ae97efd3bd35ab8c96a14b7f30e1ffe0280d38c
wlsdnr4675 <wlsdnr4675@gmail.com>
2022-03-24 12:46:12 +0900

    feat: hooks concept

--- src/react.js
+++ src/react.js
@@ -1,9 +1,14 @@
+// hooks
+const hooks = [];
+let currentComponent = 0;
+
 export class Component {
   constructor(props) {
     this.props = props;
   }
 }
 
+//Real DOM 만드는 코드
 export const createDOM = (node) => {
   if (typeof node === "string") {
     return document.createTextNode(node);
@@ -22,6 +27,20 @@ const makeProps = (props, children) => ({
   ...props,
   children: children.length === 1 ? children[0] : children,
 });
+// useState hook
+export function useState(initValue) {
+  let position = currentComponent - 1;
+  if (!hooks) {
+    hooks[position] = initValue;
+  }
+  const modifier = (newValue) => {
+    hooks[position] = newValue;
+  };
+
+  return [hooks[position], modifier];
+}
+
+//  VDOM 만드는 역할
 export const createElement = (tag, props, ...children) => {
   props = props || {};
   if (typeof tag === "function") {
@@ -29,6 +48,9 @@ export const createElement = (tag, props, ...children) => {
     if (tag.prototype instanceof Component) {
       const instance = new tag(makeProps(props, children));
       return instance.render();
+      // hook setting 제약사항 알기 위한 컨셉 코드
+      hooks[currentComponent] = null;
+      currentComponent++;
     } else {
       // 함수형 컴포넌트 분기
       if (children.length > 0) {
@@ -46,6 +68,19 @@ export const createElement = (tag, props, ...children) => {
   }
 };
 
-export const render = (vdom, container) => {
-  container.appendChild(createDOM(vdom));
-};
+// export const render = (vdom, container) => {
+//   container.appendChild(createDOM(vdom));
+// };
+
+//vdom 비교 하는 로직 초간단
+export const render = (function () {
+  let prevDOM = null;
+
+  return function (vdom, container) {
+    if (prevDOM === null) {
+      prevDOM = vdom;
+    }
+    //diff
+    container.appendChild(createDOM(vdom));
+  };
+})();
